8d4e11f293d6dd34d4081c60860dd07400f2bb4c
tplinderoth <tylerp.linderoth@gmail.com>
2022-08-19 09:47:50 -0400

    Accommodate latest angsd SFS folding output

--- genotype_snp_calling_exercises/scripts/plotSFS.R
+++ genotype_snp_calling_exercises/scripts/plotSFS.R
@@ -7,10 +7,11 @@ args <- commandArgs(trailingOnly=TRUE)
 
 sfs <- scan(args[1])
 outprefix <- args[2]
+n <- (length(sfs)-1)/2
 
-sfs = sfs[-1] # remove fixed category
+sfs = sfs[2:(n+1)] # restrict to folded categories and remove fixed class
 
 # plot
 pdf(file=paste0(outprefix,".pdf"),width=14,height=7)
-barplot(sfs, xlab="MAF", ylab="Number SNPs", names=1:length(sfs), cex.names=0.8, cex.axis=1.2, cex.lab=1.2)
+barplot(sfs, xlab="MAF", ylab="Number SNPs", names=1:n, cex.names=0.8, cex.axis=1.2, cex.lab=1.2)
 invisible(dev.off())
